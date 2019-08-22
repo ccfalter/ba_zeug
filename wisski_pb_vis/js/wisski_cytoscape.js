e121b5813ab27a84fc2d8f5f481936ec17c7478f
ccfalter <corina.lehmann@fau.de>
2019-08-22 11:15:46 +0200

    remove annotated code

--- wisski_pb_vis/js/wisski_cytoscape.js
+++ wisski_pb_vis/js/wisski_cytoscape.js
@@ -2,20 +2,21 @@
  *Initialize container "viewer" for library cytoscape.
  */
 (function ($, Drupal, drupalSettings) {
-  Drupal.behaviors.wisski_cytoscape_behaviour = {
-   attach: function (context, settings) {
-    //  alert(drupalSettings.wisski.vis.data); //alerts the value of PHP's $value
+   Drupal.behaviors.wisski_cytoscape_behaviour = {
+     attach: function (context, settings) {
+     //alert(drupalSettings.wisski.vis.data); //alerts the value of PHP's $value
      //get data from controller  
-     var json_data = drupalSettings.wisski.vis.data;
+     //var json_data = drupalSettings.wisski.vis.data;
+     $('div#viewer', context).once('wisski_cytoscape').each(function () { 
+       var json_data = drupalSettings.wisski.vis.data;
+       //console.log(json_data);
      
-     
- //   $.getJSON("https://testrakete.gnm.de/sites/default/files/2019-06/path_example.json", function (json_data) {
-     //initialize container "viewer" for library cytoscape.  
-     var cy = cytoscape({
-               container: document.getElementById("viewer"),
-               elements: json_data,
-               style: [
-                      {
+       $.getJSON("https://testrakete.gnm.de/sites/default/files/2019-06/path_example.json", function (json_data) {
+       //initialize container "viewer" for library cytoscape.  
+       var cy = cytoscape({
+                container: document.getElementById("viewer"),
+                elements: json_data,
+                style:[{
                        selector: "node",
                        style:{
                               "label": "data(label)",
@@ -42,22 +43,54 @@
                               }
                        }
                        ],
-                       layout: {
-                              name: "circle",
+                layout:{
+                       name: "circle",
+                       minNodeSpacing: 10,
+                       nodeDimensionsIncludeLabels: false,
+                       padding: 30,
+                       avoidOverlap: true,
+                       startAngle: 3 / 2 * Math.PI,
+                       clockwise: true,
+                       fit: true,
+                       boundingBox: undefined,
+                       spacingFactor: undefined,
+                       sweep: undefined,
+                       radius: undefined
+                                                                                                                                                                                                                                                         
+/*                            name: "concentric",
                               minNodeSpacing: 10,
                               nodeDimensionsIncludeLabels: false,
                               padding: 30,
+                              sweep: undefined,
+                              equidistant: false,
+                              boundingBox: undefined,
+                              spacingFactor: undefined,
                               avoidOverlap: true,
                               startAngle: 3 / 2 * Math.PI,
                               clockwise: true,
                               fit: true,
-                              radius: undefined
-                              }
-                       });
+                              radius: undefined,
+                              concentric: function( node ){ // returns numeric value for each node, placing higher nodes in levels towards the centre
+                                return node.degree();
+                              },
+                              levelWidth: function( nodes ){ // the letiation of concentric values in each level
+                                return nodes.maxDegree() / 4;
+                              },
+                              animate: false,
+                              animationDuration: 500,
+                              animationEasing: undefined,
+                              animateFilter: function ( node, i ){ return true; },
+                              ready: undefined,
+                              stop: undefined,
+                              transform: function (node, position ){ return position; } // transform a given node position. Useful for changing flow direction in discrete layouts
+*/                              
+                      }
+             });
              //alert(cy);        
              //console.log(cy);
+             
        cy.ready();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
-//      });     
-     }
-    };
- })(jQuery, Drupal, drupalSettings);
+       });     
+    }
+  };
+})(jQuery, Drupal, drupalSettings);
--- wisski_pb_vis/src/Form/CytoscapeForm.php
+++ wisski_pb_vis/src/Form/CytoscapeForm.php
@@ -32,204 +32,65 @@ use Drupal\Core\Serialization\PHP;
  */
   class CytoscapeForm extends FormBase {
               
-     /** public function __construct(FormBuilder $formBuilder) {
-        $this->formBuilder = $formBuilder;
-      }           
+   /** public function __construct(FormBuilder $formBuilder) {
+         $this->formBuilder = $formBuilder;
+       }           
                                                             
-      public static function create(ContainerInterface $container) {
-        return new static($container->get('form_builder'));
-      }                                   
+       public static function create(ContainerInterface $container) {
+         return new static($container->get('form_builder'));
+       }                                   
                                                                                                                        
-      public static function getRequest(Request $request){
-        return $request;
-      }**/
-      public function getFormId() {
-        return 'wisski_pb_vis';
-      } 
-      public function validateForm(array &$form, FormStateInterface $form_state) {
+       public static function getRequest(Request $request){
+         return $request;
+      }
+   **/
+    public function getFormId() {
+      return 'wisski_pb_vis';
+    } 
+    public function validateForm(array &$form, FormStateInterface $form_state) {
        
-      } 
-         /**Form constructor.
-         *
-         *   @param array $form
-         *   An associative array containing the structure of the form.
-         *   @param \Drupal\Core\Form\FormStateInterface $form_state
-         *   The current state of the form.
-         *   @return array
-         *   The form structure.
-         **/                                                                                                                                                                      
-      public function buildForm(array $form, FormStateInterface $form_state) {
+    } 
+    /**Form constructor.
+    *
+    *   @param array $form
+    *   An associative array containing the structure of the form.
+    *   @param \Drupal\Core\Form\FormStateInterface $form_state
+    *   The current state of the form.
+    *   @return array
+    *   The form structure.
+    **/                                                                                                                                                                      
+    public function buildForm(array $form, FormStateInterface $form_state) {
                                                                                                                                                                                                    
-         $form['description'] = ['#type' => 'item',
-                                 '#markup' => $this->t('Please enter the url of the Pathbuildertemplate File and submit.'),
-                                  ];        
-         $form['url'] = ['#type' => 'textfield',
-                         '#title' => $this->t('File URL'),
-                         '#required' => TRUE,
-                          ];
-         $form['actions'] = ['#type' => 'actions',];                 
-         $form['actions']['submit'] = ['#type' => 'submit',
+      $form['description'] = ['#type' => 'item',
+                              '#markup' => $this->t('Please enter the url of the Pathbuildertemplate File and submit.'),
+                             ];        
+      $form['url'] = ['#type' => 'textfield',
+                      '#title' => $this->t('File URL'),
+                      '#required' => TRUE,
+                     ];
+      $form['actions'] = ['#type' => 'actions',];                 
+      $form['actions']['submit'] = ['#type' => 'submit',
                                         '#value' => $this->t('Submit'),
-                                      ];
-         //$form['#attached']['drupalSettings']['wisski_cytoscape']= $url;                            
-         return $form;    
-     }
-     /**
-        * Form submission handler.
-        *
-        * @param array $form
-        *   An associative array containing the structure of the form.
-        * @param \Drupal\Core\Form\FormStateInterface $form_state
-        *   The current state of the form.
-      **/
-     public function submitForm(array &$form, FormStateInterface $form_state) {
-          // get the file and decode it
-          $url = $form_state->getValue('url');
-          $xmlfile = file_get_contents($url);
-          $form['#attached']['drupalSettings']['wisski']['vis']['data']= $xmlfile;
-          
-          //$xmlfile = file_get_contents($url); 
-          $xml2object =  simplexml_load_string($xmlfile);
-          // encode to json-string
-         // dpm($xmlfile,'Hooo');   
-          $object2json  = json_encode($xml2object);
-          // decode to php-array   
-          $json2phparray = json_decode($object2json,true);
+                                      ];                            
+      return $form;    
+    }
+    /**
+      * Form submission handler.
+      *
+      * @param array $form
+      *   An associative array containing the structure of the form.
+      * @param \Drupal\Core\Form\FormStateInterface $form_state
+      *   The current state of the form.
+    **/
+   public function submitForm(array &$form, FormStateInterface $form_state) {
+     // get the file 
+     $url = $form_state->getValue('url');
+     $xmlfile = file_get_contents($url);
+     $form['#attached']['drupalSettings']['wisski']['vis']['data']= $xmlfile;
          
-          //transform array for cytoscape visualisation
-          $nodes = array();
-          $edges = array();    
-          $path_arrays = array(); 
-          $pathentries = $json2phparray['path'];
-         // $tmp = array();
-          //get every path_array in pathbuilder_template
-          foreach ($pathentries as $pathkey => $path_table) {
-            foreach ($path_table  as $rowkey => $row){             
-                if($rowkey == 'path_array'){
-                   $path_arrays[] = $path_table['path_array'];
-                }
-            }           
-          }
-          //dpm($path_arrays, 'all Patharrays of File');
-          
-          //Concepts x and Properties y split in arrays for directed edges
-          $x_array= array();
-          $y_array = array();
-          for($i = 0; $i < count($path_arrays); $i++){            
-            //only one node in the path
-            if(count($path_arrays[$i]) == 1){
-               $x_array[$i] = ['data' =>['id' => $path_arrays[$i]['x'], 'label' => $path_arrays[$i]['x']]];
-              // dpm($x_array[$i], 'Fall 1');
-            }
-            if(count($path_arrays[$i]['x']) == 2){
-               $x_array[$i] =[
-                                ['data' 
-                                 => ['id' => $path_arrays[$i]['x'][0],
-                                   'label'=> $path_arrays[$i]['x'][0]
-                                    ]
-                                ],
-                                ['data' 
-                                  =>['id' => $path_arrays[$i]['x'][1], 
-                                   'label'=> $path_arrays[$i]['x'][1]
-                                   ]
-                                ]
-                             ];
-               
-               $y_array[$i] = ['data' 
-                              =>['id'=> $path_arrays[$i]['y'], 
-                                 'source'=> $path_arrays[$i]['x'][0],
-                                 'target'=>$path_arrays[$i]['x'][1]
-                                 ]
-                             ];
-             //  dpm($x_array[$i], 'Fall 2');
-             //  dpm($y_array[$i], 'Fall 2');
-            }
-            else{ //pathlength >=3
-              for($j = 0; $j < count($path_arrays[$i]['x']); $j++){
-                
-                  $x_array[$i] = [ 'data' =>
-                                   ['id'=>$path_arrays[$i]['x'][$j],
-                                    'label'=> $path_arrays[$i]['x'][$j]
-                                    ]
-                                 ];
-                
-                  $y_array[$i] = ['data'
-                                  =>['id'=> $path_arrays[$i]['y'][$j],
-                                     'source'=> $path_arrays[$i]['x'][$j],
-                                     'target'=> $x_path_arrays[$i]['x'][$j+1]  
-                                    ]
-                                 ];
-              //dpm($x_array[$i], 'Fall 3');
-             // dpm($y_array[$i], 'Fall 3');  
-              }
-            }
-            //  $count = 0;
-              //$x_array[] = $path_arrays[$i]['x'];
-              //$y_array[] = $path_arrays[$i]['y'];
-             /** if($y_array[$i]!='NULL'){
-                dpm($x_array[$i]['y'], 'dibadiadu');
-               // dpm($c++, 'counter');
-                while(count($y_array[$i]['y']) < $count){
-                  $edges[] = [array('id'=>$y_array[$i]['y'][$count],'source'=>$x_array[$i]['x'][$count], 'target'=>$x_array[$i]['x'][$count+1])]; 
-                  $count++;
-                 }
-              }
-         **/
-          }
-         $nodes = $x_array;
-         $edges = $y_array;
-         $jsonforcytoscape = ['nodes'=> $nodes, 'edges' => $edges];
-         //dpm($jsonforcytoscape, 'am I CytoJSON?');
-         $json_file = json_encode($jsonforcytoscape);
-         //dpm($json_file, '?'); 
-         /** foreach($x_array as $k => $val){
-            if(is_array($val)){
-             //dpm($val, 'x_val');
-             foreach($val as $key => $value){
-             dpm($value, 'values');
-             $nodes = [array('id' => $value, 'label' => $value)];
-             }
-            }else{
-             
-            }
-          }**/
-          //dpm($x_array,'X');
-          //dpm($y_array, 'Y');
-          //array-key of path_arrays is an integer
-          /**foreach($path_arrays as $key => $value){
-            for($i = 0; $i < count($path_arrays); $i++){
-              dpm($path_arrays[$i], 'hhhh');
-                //path consists at least of one property
-                if(count($path_arrays[$i]) > 1){
-                    
-                }
-                                  
-            } 
-                                            
-          }**/
-          
-          //dpm($path_arrays, 'Hey, path arrays');
-          /**foreach($path_arrays as $x_y => $ontology_class_property){
-            
-            if($x_y =='x'){
-              
-              $nodes[] = array("id" => $ontology_class_property, "label"=> $ontology_class_property);
-            }
-            elseif($x_y =='y'){
-            }
-          } dpm($nodes, 'Hey, nodes');**/
-          
-          //encode to json
-          
-          //$url = Url::fromUserInput('/wisski/pb_vis');
-          //$form_state->setRedirect($url);
-          //$response = new JsonResponse($json_file);
-          $form['#attached']['drupalSettings']['wisski']['vis']['data']= $json_file;
-          $form['#attached']['library'][] = 'wisski_pb_vis/vis';
-          
-          dpm($form, "yay?");
-          return FALSE;           
-          $form_state->setRedirect('wisski_pb_vis.mainpage');
-     }
+     //$response = new JsonResponse($json_file);
+     $form['#attached']['library'][] = 'wisski_pb_vis/vis';          
+     $form_state->setRedirect('wisski_pb_vis.mainpage');
+   }
                                                                           
  }                                                                                                                                         
